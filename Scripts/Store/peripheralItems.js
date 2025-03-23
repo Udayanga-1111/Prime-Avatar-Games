4fc25e4aa767ce7e896924d0266a857849b81a22
UdayangaDilshan <udayangadeniyagoda2@gmail.com>
2025-03-23 23:26:02 +0530

    added jason for peripheral items

--- Scripts/Store/peripheralItems.js
+++ Scripts/Store/peripheralItems.js
@@ -1,51 +1,35 @@
-// define the class for peripheral items.
-
-class item {
-    constructor(
-        itemNo,
-        itemClass,
-        imgSrc,
-        itemName,
-        itemPrice,
-        ItemStatus,
-    ) {
-        this.itemNo = itemNo,
-        this.itemClass = itemClass;
-        this.imgSrc = imgSrc;
-        this.itemName = itemName;
-        this.itemPrice = itemPrice;
-        this.ItemStatus = ItemStatus;
-    }
-
-    cardCreator = function () {
+const abc = [];
+const addToHtml = function () {
+    abc.forEach(product => {
         let aDiv = document.createElement("a");
         aDiv.classList.add("card");
-        aDiv.classList.add(this.itemClass)
+        aDiv.classList.add(product.itemClass)
         aDiv.setAttribute("href", "#store");
         aDiv.innerHTML =
-            `<img src="${this.imgSrc}" alt="Item Picture" class="item-pic">
+            `<img src="${product.imgSrc}" alt="Item Picture" class="item-pic">
     
             <section class="item-details">
-                <h2 class="item-name">${this.itemName}</h2>
-                <h4 class="price">$${this.itemPrice}</h4>
-                <h4 class="extra">${this.ItemStatus}</h4>
-                <button class="addToCart" id="${this.itemNo}">+</button>
+                <h2 class="item-name">${product.itemName}</h2>
+                <h4 class="price">$${product.itemPrice}</h4>
+                <h4 class="extra">${product.itemStatus}</h4>
+                <button class="addToCart" id="${product.itemNo}">+</button>
             </section>`;
         
         let itemSection = document.querySelector(".item-list"); // Assuming 'item-list' is a class
         itemSection.append(aDiv);
-    };
-}
+    })
+};
 
-// 01
 
-const item1 = new item(
-    "item1",
-    "mouse",
-    "./Stylesheets/Images/Peripherals-Images/Glorious-Model_D.webp",
-    "Glorious Model D",
-    "61.39",
-    "Available"
-)
+const initApp = () => {
+    fetch('./Scripts/Store/peripherals.json') // Ensure the file is in the root directory of the server
+        .then(response => response.json())
+        .then(data => {
+            abc.push(...data);
+            console.log(abc);
+            
+            addToHtml();
+        })
+};
 
-item1.cardCreator();
+initApp();
