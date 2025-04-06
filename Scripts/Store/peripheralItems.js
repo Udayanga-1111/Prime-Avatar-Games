dbee07d548972da15b1c0392fa5662291bcffbfc
UdayangaDilshan <udayangadeniyagoda2@gmail.com>
2025-04-06 09:17:08 +0530

    peripherals edited

--- Scripts/Store/peripheralItems.js
+++ Scripts/Store/peripheralItems.js
@@ -1,19 +1,19 @@
 const abc = [];
 const addToHtml = function () {
     abc.forEach(product => {
-        let aDiv = document.createElement("a");
+        let aDiv = document.createElement("div");
         aDiv.classList.add("card");
         aDiv.classList.add(product.itemClass)
-        aDiv.setAttribute("href", "#store");
         aDiv.innerHTML =
             `<img src="${product.imgSrc}" alt="Item Picture" class="item-pic">
     
             <section class="item-details">
                 <h2 class="item-name">${product.itemName}</h2>
                 <h4 class="price">$${product.itemPrice}</h4>
-                <h4 class="extra">${product.itemStatus}</h4>
-                <button class="addToCart" id="${product.itemNo}">+</button>
-            </section>`;
+                <button class="addToCart none" id="${product.itemNo}">Add To Cart</button>
+            </section>
+            
+            <button class="addToCart none2" id="${product.itemNo}">Add To Cart</button>`;
         
         let itemSection = document.querySelector(".item-list"); // Assuming 'item-list' is a class
         itemSection.append(aDiv);
@@ -25,7 +25,7 @@ const initApp = () => {
     fetch('./Scripts/Store/peripherals.json') // Ensure the file is in the root directory of the server
         .then(response => response.json())
         .then(data => {
-            abc.push(...data);
+            abc.push(...data.peripherals); // Spread operator to push all items into the array
             
             addToHtml();
         })
