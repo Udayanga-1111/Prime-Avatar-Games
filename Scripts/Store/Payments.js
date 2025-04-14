bcbed75bc93a17f8ab06fa4067b55d71c34ce071
UdayangaDilshan <udayangadeniyagoda2@gmail.com>
2025-04-14 11:51:18 +0530

    Payment Function

--- Scripts/Store/Payments.js
+++ Scripts/Store/Payments.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", optionCheck);
-const personalDetailsForm = document.getElementById("personalDetails");;
+const theForm = document.getElementById("paymentForm");
 const paymentOpt = document.getElementsByName("paymentMethod");
 const visaOption = document.getElementById("visaPaymentDetails");
 const paypalOption = document.getElementById("paypalPaymentDetails");
+
+const cardInputs = document.querySelectorAll(".visaInfo");
+const paypalInputs = document.querySelectorAll(".paypalInfo");
+
 const visaOptionLabel = document.getElementById("visaLabel");
 const paypalOptionLabel = document.getElementById("paypalLabel");
 const payBtn = document.getElementById("payBtn")
@@ -12,6 +16,9 @@ const totalItems = document.getElementById("totalItems");
 const subTotal = document.getElementById("subTotal");
 const discount = document.getElementById("discount");
 const tax = document.getElementById("tax");
+
+// Default Values ==============================================================
+var payOption = "visa";
 
 // Payment Option Selection Checking ===========================================
 paymentOpt.forEach(opt => opt.addEventListener("change", optionCheck));
@@ -23,7 +30,10 @@ function optionCheck() {
             paypalOption.style.display = "none";
             visaOptionLabel.style.backgroundColor = "rgb(21, 127, 146)"
             paypalOptionLabel.style.backgroundColor = "transparent"
-            window.payOption = "visa";
+            payOption = "visa";
+
+            cardInputs.forEach(input => input.setAttribute("required",true));
+            paypalInputs.forEach(input => input.removeAttribute("required"));
             break;
         
         case this.value === "paypal":
@@ -31,7 +41,10 @@ function optionCheck() {
             visaOption.style.display = "none";
             paypalOptionLabel.style.backgroundColor = "rgb(21, 127, 146)"
             visaOptionLabel.style.backgroundColor = "transparent"
-            window.payOption = "paypal";
+            payOption = "paypal";
+
+            paypalInputs.forEach(input => input.setAttribute("required", true));
+            cardInputs.forEach(input => input.removeAttribute("required"));
             break;
         
         default:
@@ -39,7 +52,9 @@ function optionCheck() {
             paypalOption.style.display = "none";
             visaOptionLabel.style.backgroundColor = "rgb(21, 127, 146)"
             paypalOptionLabel.style.backgroundColor = "transparent"
-            window.payOption = "visa";
+            payOption = "visa";
+            cardInputs.forEach(input => input.setAttribute("required", true));
+            paypalInputs.forEach(input => input.removeAttribute("required"));
     }
 };
 
@@ -58,17 +73,17 @@ Total.innerText = finalPrice;
 
 // Payment Alert ===============================================================
 
-//Issue is here
+// //Issue is here
 payBtn.addEventListener("click", payment);
 
 function payment(event) {
-    if (paymentOpt.value === "visa") {
-        if (personalDetailsForm.checkValidity() && visaOption.checkValidity()) {
+    if (payOption === "visa") {
+        if(theForm.checkValidity()) {
             event.preventDefault();
             alert("thanks for the payment, Your order will shipped within 3 days")
         }
-    } else if (paymentOpt.value === "paypal") {
-        if (personalDetailsForm.checkValidity() && paypalOption.checkValidity()) {
+    } else if (payOption === "paypal") {
+        if (theForm.checkValidity()) {
             event.preventDefault();
             alert("thanks for the payment, Your order will shipped within 3 days")
         }
